9e4997966e623b7c28cc5901efc39dad8ac06ee5
Brad W <bwittenberg1@gmail.com>
2024-02-01 19:46:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.body.children[1].children[0].href = 'https://www.google.ca';
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = 'https://www.red.com';
+
+document.querySelector('a'); // this is a CSS element selector
+anchorElement.href = 'https://www.wittentechnology.com';
